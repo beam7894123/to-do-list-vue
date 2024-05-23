6f0251bf873831e1cffaf706f053e9ee6951e4f6
beam7894123 <susit.t@ku.th>
2024-05-23 17:50:17 +0700

    Add dotenv and axios

---
+++ src/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,  // This should read the VITE_API_BASE_URL from the .env file
+});
+
+console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL);  // Debug: Check if the URL is loaded
+
+export default apiClient;
--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,21 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dotenv from 'dotenv';
 
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    host: process.env.VITE_SERVER_URL || 'localhost',
+    port: parseInt(process.env.VITE_SERVER_PORT, 10) || 3000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    port: 8080,
   },
 })
