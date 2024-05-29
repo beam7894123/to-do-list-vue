f19113262ec0420f56eecde35d68f369662b891f
beam7894123 <susit.t@ku.th>
2024-05-29 13:04:13 +0700

    Fix deploy get Blank page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL), // <- This is for local production
+  history: createWebHashHistory(import.meta.env.BASE_URL), // <- This is for git pages
   routes: [
     // {
     //   path: '/',
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
   ],
